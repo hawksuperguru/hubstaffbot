6a57cb3aed377aa31a3ff14f7493a64ba82a19e2
hawksuperguru <falcon.laravguru@yandex.com>
2018-06-11 00:04:47 -0700

    Change WSGI File

--- hubstaffbot/wsgi.py
+++ hubstaffbot/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hubstaffbot.settings")
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
